9bca0d20983a95dce323c906c037d0471b3fa7ba
Michiel Ryvers <michiel.ryvers@itenium.be>
2022-06-06 14:55:48 +0200

    Add baseline + IFTTT config from WiFiManager

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,37 @@
 #include <WebServer.h>
 #include <WiFiManager.h>
 #include <HTTPClient.h>
+#include <Preferences.h>
+#include <time.h>
+#include "icons.h"
 
+// Display
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
-
 #define OLED_RESET 18
-
-WiFiManager wm;
-Adafruit_SGP30 sgp;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// WiFiManager
+WiFiManager wm;
+char wm_ifttt_url[100];
+String ifttt_url = "";
+WiFiManagerParameter custom_ifttt_url("ifttturl", "IFTTT Webhook URL", wm_ifttt_url, 100);
+
+// SGP30 CO2 sensor
+Adafruit_SGP30 sgp;
+
+// Preferences
+Preferences preferences;
+String pref_ifttt_url;
+uint16_t TVOC_base = 0;
+uint16_t eCO2_base = 0;
+
+// Time
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 3600;
+const int daylightOffset_sec = 3600;
+
+// Methods
 void resetScreen();
 void measureLoop();
 void drawProgressBar(int16_t maxValue, int16_t currentValue);
@@ -27,63 +48,7 @@ void showBootscreen();
 void configModeCallback(WiFiManager *myWiFiManager);
 bool checkConnection();
 void sendNotification();
-
-unsigned char wifi_icon16x16[] =
-    {
-        0b00000000, 0b00000000, //
-        0b00000111, 0b11100000, //      ######
-        0b00011111, 0b11111000, //    ##########
-        0b00111111, 0b11111100, //   ############
-        0b01110000, 0b00001110, //  ###        ###
-        0b01100111, 0b11100110, //  ##  ######  ##
-        0b00001111, 0b11110000, //     ########
-        0b00011000, 0b00011000, //    ##      ##
-        0b00000011, 0b11000000, //       ####
-        0b00000111, 0b11100000, //      ######
-        0b00000100, 0b00100000, //      #    #
-        0b00000001, 0b10000000, //        ##
-        0b00000001, 0b10000000, //        ##
-        0b00000000, 0b00000000, //
-        0b00000000, 0b00000000, //
-        0b00000000, 0b00000000, //
-};
-
-unsigned char warning_icon16x16[] =
-    {
-        0b00000000, 0b10000000, //         #
-        0b00000001, 0b11000000, //        ###
-        0b00000001, 0b11000000, //        ###
-        0b00000011, 0b11100000, //       #####
-        0b00000011, 0b01100000, //       ## ##
-        0b00000111, 0b01110000, //      ### ###
-        0b00000110, 0b00110000, //      ##   ##
-        0b00001110, 0b10111000, //     ### # ###
-        0b00001100, 0b10011000, //     ##  #  ##
-        0b00011100, 0b10011100, //    ###  #  ###
-        0b00011000, 0b10001100, //    ##   #   ##
-        0b00111000, 0b00001110, //   ###       ###
-        0b00110000, 0b10000110, //   ##    #    ##
-        0b01111111, 0b11111111, //  ###############
-        0b01111111, 0b11111111, //  ###############
-        0b00000000, 0b00000000, //
-};
-
-// 'CO2 icon', 50x32px
-const unsigned char CO2_icon[] PROGMEM = {
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0xf8, 0x00, 0x3f, 0x00, 0x00, 0x00, 0x07, 0xfe, 0x00, 0xff,
-    0xc0, 0x00, 0x00, 0x0f, 0xff, 0x01, 0xff, 0xe0, 0x00, 0x00, 0x1f, 0x0f, 0x03, 0xe1, 0xf0, 0x00,
-    0x00, 0x1c, 0x07, 0x83, 0x80, 0x70, 0x00, 0x00, 0x38, 0x03, 0x87, 0x80, 0x78, 0x00, 0x00, 0x38,
-    0x03, 0xc7, 0x00, 0x38, 0x00, 0x00, 0x78, 0x01, 0xcf, 0x00, 0x3c, 0x00, 0x00, 0x70, 0x01, 0x8e,
-    0x00, 0x1c, 0x00, 0x00, 0x70, 0x00, 0x0e, 0x00, 0x1c, 0x00, 0x00, 0x70, 0x00, 0x0e, 0x00, 0x1c,
-    0x00, 0x00, 0x70, 0x00, 0x0e, 0x00, 0x1c, 0x00, 0x00, 0x70, 0x00, 0x0e, 0x00, 0x1c, 0x00, 0x00,
-    0x70, 0x00, 0x0e, 0x00, 0x1c, 0x00, 0x00, 0x70, 0x01, 0x8e, 0x00, 0x1c, 0x3c, 0x00, 0x70, 0x01,
-    0xce, 0x00, 0x1c, 0x7e, 0x00, 0x78, 0x01, 0xcf, 0x00, 0x3c, 0xe7, 0x00, 0x38, 0x03, 0x87, 0x00,
-    0x38, 0xc3, 0x00, 0x38, 0x03, 0x87, 0x80, 0x78, 0x03, 0x00, 0x3c, 0x07, 0x83, 0x80, 0x70, 0x03,
-    0x00, 0x1f, 0x0f, 0x03, 0xe1, 0xf0, 0x06, 0x00, 0x0f, 0xfe, 0x01, 0xff, 0xe0, 0x0e, 0x00, 0x07,
-    0xfc, 0x00, 0xff, 0xc0, 0x1c, 0x00, 0x01, 0xf0, 0x00, 0x3f, 0x00, 0x38, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+void setIFTTTUrl();
 
 void setup()
 {
@@ -92,9 +57,26 @@ void setup()
     Serial.setDebugOutput(true);
     delay(3000);
 
+    // For testing purposes of the WiFiManager uncomment the next line, this gives you a fresh start each boot
+    wm.resetSettings();
+
+    // Set up the time on our ESP32
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+    // Initialize the Preferences library and check for existing values
+    preferences.begin("co2meter", false);
+    TVOC_base = preferences.getUShort("TVOC_base", 0);
+    eCO2_base = preferences.getUShort("eCO2_base", 0);
+    pref_ifttt_url = preferences.getString("IFTTT_url", "");
+
+    // Add a "IFTTT Webhook URL" option to the WiFiManager
+    wm.addParameter(&custom_ifttt_url);
+
+    // Start the display
     display.begin();
     resetScreen();
 
+    // Check if we can talk to our CO2 sensor
     if (!sgp.begin())
     {
         display.clearDisplay();
@@ -104,6 +86,13 @@ void setup()
             ;
     }
 
+    // If we have previously saved baseline values for the TVOC and CO2 values, set them here
+    if (TVOC_base != 0 && eCO2_base != 0)
+    {
+        sgp.setIAQBaseline(eCO2_base, TVOC_base);
+    }
+
+    // Bootscreen allows the CO2 sensor some time to warm up
     showBootscreen();
 
     display.setFont();
@@ -118,85 +107,96 @@ void loop()
     }
     else
     {
+        Serial.println("Not connected to the network, entering checkConnection");
+        // If we're not connected to the WiFi check for a saved network or open the captive portal
         checkConnection();
+        // We should be connected, check if the user passed a IFTTT URL
+        setIFTTTUrl();
     }
-}
-
-void resetScreen()
-{
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.setFont();
-    display.cp437(true);
-    display.setTextSize(1);
 }
 
 bool checkConnection()
 {
-    if (wm.getWiFiSSID() != "")
+    String prefix = "CO2-";
+    String hostString = String(WIFI_getChipId(), HEX);
+    hostString.toUpperCase();
+    prefix.concat(hostString);
+    char ap_name[50];
+    prefix.toCharArray(ap_name, 50);
+
+    if (wm.getWiFiIsSaved())
     {
         resetScreen();
         display.println("Found saved network");
         display.println(wm.getWiFiSSID());
         display.println("Trying to connect");
         display.display();
-        return wm.autoConnect();
+        return wm.autoConnect(ap_name);
     }
     else
     {
         resetScreen();
-        display.println("Connect to CO-32");
+        display.print("Connect to ");
+        display.println(ap_name);
         display.println("to continue setup");
         display.display();
-        return wm.startConfigPortal("CO-32");
+        return wm.startConfigPortal(ap_name);
     }
 }
 
-void sendNotification()
+void setIFTTTUrl()
 {
-    HTTPClient http;
+    // Get value from WiFiManager
+    strcpy(wm_ifttt_url, custom_ifttt_url.getValue());
+    String wm_ifttt_url_string = String(wm_ifttt_url);
 
-    String serverPath = "https://maker.ifttt.com/trigger/co2_too_high/json/with/key/dpXn2QAUsLyEZ_uLHxKHG4";
+    Serial.println("Setting the IFTTT URL");
+    Serial.println("Value from WiFiManager: ");
+    Serial.println(wm_ifttt_url_string);
+    Serial.println("Value in Preferences: ");
+    Serial.println(pref_ifttt_url);
 
-    // Your Domain name with URL path or IP address with path
-    http.begin(serverPath.c_str());
-
-    // Send HTTP GET request
-    int httpResponseCode = http.GET();
-
-    http.end();
+    // The WiFiManager passed us a IFTTT URL
+    if (wm_ifttt_url_string != "")
+    {
+        ifttt_url = wm_ifttt_url_string;
+        preferences.putString("IFTTT_url", ifttt_url);
+    }
+    // If no URL was passed, check for a previously saved one in the Preferences
+    else if (pref_ifttt_url != "")
+    {
+        ifttt_url = pref_ifttt_url;
+    }
+    Serial.println("Used URL: ");
+    Serial.println(ifttt_url);
 }
 
 int counter = 0;
-uint16_t TVOC_base = 0;
-uint16_t eCO2_base = 0;
 ulong lastNotification = 0;
 void measureLoop()
 {
     counter++;
     resetScreen();
 
+    // Try to get data from the SGP30 sensor
     if (!sgp.IAQmeasure())
     {
         display.println("Measurement failed");
         display.display();
+        delay(1000);
         return;
     }
 
-    if (counter == 20)
-    {
-        // SET baseline values
-        counter = 0;
-    }
-
+    // Display the data on the screen
     display.drawBitmap(0, 0, CO2_icon, 50, 32, 1);
     display.setTextSize(1);
     display.setFont(&FreeSans12pt7b);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(54, 24);
     display.print(sgp.eCO2);
-
     display.drawBitmap(112, 0, wifi_icon16x16, 16, 16, 1);
+
+    // Check if the CO2 value is above 800 and maybe send a notification
     if (sgp.eCO2 > 800)
     {
         if (counter % 2 == 0)
@@ -207,19 +207,54 @@ void measureLoop()
         // Only send notification when the last one is more than 10 minutes ago
         if (lastNotification == 0 || millis() - lastNotification > 600000)
         {
-            Serial.println("SENDING NOTIFICATION");
-            Serial.print("NOW: ");
-            Serial.println(millis());
-            Serial.print("LAST NOTIFICATION: ");
-            Serial.println(lastNotification);
             sendNotification();
             lastNotification = millis();
         }
     }
 
+    // Every minute we read out the baseline values and save them to EEPROM
+    if (counter == 60)
+    {
+        if (!sgp.getIAQBaseline(&eCO2_base, &TVOC_base))
+        {
+            Serial.println("Failed to get baseline readings");
+        }
+        else
+        {
+            preferences.putUShort("TVOC_base", TVOC_base);
+            preferences.putUShort("eCO2_base", eCO2_base);
+        }
+        counter = 0;
+    }
+
     display.display();
 
     delay(1000);
+}
+
+void sendNotification()
+{
+    // Only send notification if we have a IFTTT url
+    if (ifttt_url != "")
+    {
+        HTTPClient http;
+
+        http.begin(ifttt_url);
+        Serial.println("Sending GET request to: ");
+        Serial.println(ifttt_url);
+        int httpResponseCode = http.GET();
+
+        http.end();
+    }
+}
+
+void resetScreen()
+{
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setFont();
+    display.cp437(true);
+    display.setTextSize(1);
 }
 
 void showBootscreen()
